f698d41352b6c01c7f4c6a078fa8e4e6d93ff2c0
suryadev <somasuryadev6@gmail.com>
2025-04-03 10:31:24 +0530

    did changes for deployment: fixed error var in processed api

--- src/app/api/processed/[filename]/route.ts
+++ src/app/api/processed/[filename]/route.ts
@@ -15,7 +15,7 @@ export async function GET(req: NextRequest, { params }: { params: Promise<{ file
             },
         });
     } catch (error) {
-        console.log("error");
+        console.log(error);
         return NextResponse.json("error", { status: 500 });
     }
 }
